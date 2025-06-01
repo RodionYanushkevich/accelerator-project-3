1c3c6d0a189225f5910e2d84898181d45ac412ab
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-06-01 19:14:17 +0700

    news links swiper

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,6 @@
 // https://swiperjs.com/get-started#installation
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import 'swiper/css';
+
+import '../js/modules/news/link-swiper.js';
---
+++ source/js/modules/news/link-swiper.js
@@ -0,0 +1,53 @@
+import Swiper from 'swiper';
+import { Navigation, A11y } from 'swiper/modules';
+
+const linkSwiperContainer = document.querySelector('.news__links-swiper');
+
+const swiperClassListToggle = (swiperContainer) => {
+  swiperContainer.classList.toggle('swiper-container');
+  const swiperWrapper = swiperContainer.children[0];
+  swiperWrapper.classList.toggle('swiper-wrapper');
+
+  const swiperSlidesArray = Array.from(swiperWrapper.children);
+  swiperSlidesArray.forEach((slide) => {
+    slide.classList.toggle('swiper-slide');
+  });
+};
+
+
+let linksSwiper = null;
+
+const initSwiper = () => {
+  linksSwiper = new Swiper(linkSwiperContainer, {
+    modules: [A11y],
+    speed: 300,
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    // spaceBetween: 10,
+    // breakpoints: {
+    //   768: {
+    //     slidesPerView: 3,
+    //   },
+    // },
+  });
+};
+
+const destroySwiper = () => {
+  if (linksSwiper) {
+    linksSwiper.destroy(true, true);
+    linksSwiper = null;
+    swiperClassListToggle(linkSwiperContainer);
+  }
+};
+
+const resizeListener = () => {
+  if (window.innerWidth < 768 && !linksSwiper) {
+    swiperClassListToggle(linkSwiperContainer);
+    initSwiper();
+  } else if (window.innerWidth >= 768 && linksSwiper) {
+    destroySwiper();
+  }
+};
+
+window.addEventListener('resize', resizeListener);
+resizeListener();
