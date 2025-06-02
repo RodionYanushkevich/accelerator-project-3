c1b10acaaeb5507e808ddb5058da7f657c874635
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-06-02 13:45:43 +0700

    accordion pp

--- source/js/modules/faq/accordion.js
+++ source/js/modules/faq/accordion.js
@@ -1,44 +1,35 @@
-const accordionButtons = document.querySelectorAll('.accordion__button');
 const accordions = document.querySelectorAll('.accordion');
 
-// const descriprionsCheck = (accordion) => {
-//   const accordionCurrentDescriptions = accordion.querySelectorAll('p');
-//   const descriprionsCount = accordionCurrentDescriptions.length;
+const descriprionsCheck = (accordion) => {
+  const accordionCurrentDescriptions = accordion.querySelectorAll('p');
+  const descriprionsCount = accordionCurrentDescriptions.length;
 
-//   if (descriprionsCount > 1) {
-//     if (accordion.classList.contains('accordion--hidden')) {
-//       accordion.style.gridTemplateRows = `min-content repeat(${descriprionsCount}, 0fr)`;
-//     }
-//     if (accordion.classList.contains('accordion--unhidden')){
-//       accordion.style.gridTemplateRows = `min-content repeat(${descriprionsCount}, 1fr)`;
-//     }
-//   }
-// };
+  if (descriprionsCount > 1) {
+    if (!accordion.classList.contains('accordion--open')) {
+      accordion.style.gridTemplateRows = `min-content repeat(${descriprionsCount}, 0fr)`;
+    }
+    if (accordion.classList.contains('accordion--open')) {
+      accordion.style.gridTemplateRows = `min-content repeat(${descriprionsCount}, 1fr)`;
+    }
+  }
+};
 
-// const hideShowToggle = (accordionCurrent) => {
-//   accordionCurrent.classList.toggle('accordion--hidden');
-//   accordionCurrent.classList.toggle('accordion--unhidden');
+const classListToggle = (accordion, button) => {
+  accordion.classList.toggle('accordion--open');
+  button.classList.toggle('accordion__button--open');
+  descriprionsCheck(accordion);
+};
 
-// };
+accordions.forEach((accordion) => {
+  const button = accordion.children[1];
+  descriprionsCheck(accordion);
 
-// accordions.forEach((accordion) => {
-//   if (!accordion.classList.contains('accordion--unhidden')) {
-//     accordion.classList.add('accordion--hidden');
-//   }
-//   descriprionsCheck(accordion);
-// });
+  accordion.addEventListener('click', () => {
+    classListToggle(accordion, button);
+  });
 
-
-accordionButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const accordionCurrent = button.parentNode;
-
-    // hideShowToggle(accordionCurrent);
-
-    // if (accordionCurrent.querySelectorAll('p').length > 1) {
-    //   descriprionsCheck(accordionCurrent);
-    // }
-    accordionCurrent.classList.toggle('accordion--open');
-    button.classList.toggle('accordion__button--open');
+  button.addEventListener('click', (e) => {
+    e.stopPropagation();
+    classListToggle(accordion, button);
   });
 });
