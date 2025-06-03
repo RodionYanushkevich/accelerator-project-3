f42733d0fbbe3c09b47703f29d18aa34cf6e2a15
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-06-03 13:43:38 +0700

    hero swiper

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,6 @@
 // import {Navigation, Pagination} from "swiper/modules";
 import 'swiper/css';
 
+import '../js/modules/hero/hero-swiper.js';
 import '../js/modules/news/link-swiper.js';
 import '../js/modules/faq/accordion.js';
-import '../js/modules/hero/hero-swiper.js';
--- source/js/modules/hero/hero-swiper.js
+++ source/js/modules/hero/hero-swiper.js
@@ -1,58 +1,57 @@
 import Swiper from 'swiper';
-
-import { EffectFade, Pagination, A11y } from 'swiper/modules';
-
+import { EffectFade, A11y } from 'swiper/modules';
 import 'swiper/css/effect-fade';
 
-const paginationContainer = document.querySelector('.hero__pagination.swiper-pagination');
+const paginationBulletsCreate = (swiper) => {
+  const activeSlide = swiper.slides[swiper.activeIndex];
+  const paginationParent = activeSlide.querySelector('.swiper-pagination');
+
+  paginationParent.innerHTML = '';
+
+  for (let i = 0; i < swiper.slides.length; i++) {
+    const bullet = document.createElement('button');
+    bullet.setAttribute('type', 'button');
+    bullet.classList.add('hero-card__bullet', 'swiper-pagination-bullet');
+
+    if (i === swiper.realIndex) {
+      bullet.classList.add('swiper-pagination-bullet-active');
+    }
+
+    const buttonDescription = document.createElement('span');
+    buttonDescription.classList.add('visually-hidden');
+    buttonDescription.textContent = `Перейти к слайду ${i + 1}`;
+    bullet.appendChild(buttonDescription);
+
+    bullet.addEventListener('click', () => {
+      swiper.slideToLoop(i);
+    });
+
+    paginationParent.appendChild(bullet);
+  }
+};
 
 new Swiper('.hero__swiper', {
-  modules: [EffectFade, Pagination, A11y],
+  modules: [EffectFade, A11y],
   loop: true,
   speed: 300,
   slidesPerView: 1,
-  // initialSlide: 3,
   effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
-  },
-  a11y: {
-    enabled: true,
-  },
+  fadeEffect: { crossFade: true },
+  a11y: { enabled: true },
   autoHeight: true,
-  // on: {
-  //   init: function () {
-  //     this.slides.forEach((slide) => {
-  //       if (!slide.classList.contains('swiper-slide-active')) {
-  //         slide.children[0].children[2].setAttribute('tabindex', '-1');
-  //       }
-  //     });
-  //   },
-  //   slideChange: function () {
-  //     this.slides.forEach((slide) => {
-  //       slide.children[0].children[2].setAttribute('tabindex', '-1');
-  //     });
 
-  //     const activeSlide = this.slides[this.activeIndex];
-  //     activeSlide.children[0].children[2].setAttribute('tabindex', '0');
-  //   },
-  // },
-  pagination: {
-    el: paginationContainer,
-    type: 'bullets',
-    clickable: true,
-    renderBullet: function (index) {
-      return `<button class=" hero__pagination-bullet swiper-pagination-bullet" type="button">
-      <span class="visually-hidden">кнопка пагинации туров слайд № ${index + 1}
-      </span>
-      </button>`;
+  on: {
+    init: function () {
+      paginationBulletsCreate(this);
     },
+    slideChange: function () {
+      paginationBulletsCreate(this);
+    }
   },
   breakpoints: {
     1440: {
       allowTouchMove: false,
-      simulateTouch: false,
+      simulateTouch: false
     }
   }
 });
-
