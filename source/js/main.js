f8b5bcaf353acba3d85f663cb1f6fa07fd474c3f
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-06-04 22:20:39 +0700

    dropdown menu

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@
 // import {Navigation, Pagination} from "swiper/modules";
 import 'swiper/css';
 
+import '../js/modules/header/dropdown-menu.js';
 import '../js/modules/hero/hero-swiper.js';
 import '../js/modules/news/link-swiper.js';
 import '../js/modules/faq/accordion.js';
